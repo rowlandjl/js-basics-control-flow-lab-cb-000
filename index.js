0209330e3cd7562d7c2088d2b6932fcb320b058b
Jessica Rowland <rowlandjl@vcu.edu>
2019-11-09 15:30:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function scuberGreetingForFeet(distance){
   if (distance > 2500) {
     return 'No can do.';
   } else if (distance > 2000) {
-    return 'I will gladly take 30 bucks.';
+    return 'I will gladly take your thirty bucks.';
   } else {
     return 'This one is on me.';
   }
